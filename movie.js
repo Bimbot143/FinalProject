187c5364b6a9f045ad3cdfea1bfb4d05775aab72
Vincent Salazar <127659058+Bimbot143@users.noreply.github.com>
2023-12-19 23:57:47 +0800

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,23 @@
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .imagelist");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+
+    const handleSlidebuttons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "block";
+    }
+
+    imageList.addEventListener("scroll", () => {
+        handleSlidebuttons();
+    });
+}
+
+window.addEventListener("load", initSlider);
---
+++ music-list.js
@@ -0,0 +1,63 @@
+let allMusic = [
+
+    {
+        name: "Sunflower",
+        artist: "Post Malone",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+    name: "Ao No Sumika",
+    artist: "Tatsuya Kitani",
+    img: "music-2",
+    src: "music-2"
+    },
+    {
+    name: "Mundo",
+    artist: " IV of Spades",
+    img: "music-3",
+    src: "music-3"
+    },
+    {
+    name: "[SHOCK]",
+    artist: "Ayase",
+    img: "music-4",
+    src: "music-4"
+    },
+    {
+    name: "Enemy",
+    artist: "Imagine Dragons",
+    img: "music-5",
+    src: "music-5"
+    },
+    {
+    name: "Annihilate",
+    artist: "Metro Boomin",
+    img: "music-6",
+    src: "music-6"
+    },
+    {
+        name: "Araw-Araw",
+        artist: "Ben&Ben",
+        img: "music-7",
+        src: "music-7"
+        },
+        {
+            name: "Snowman",
+            artist: "Sia",
+            img: "music-8",
+            src: "music-8"
+            },
+            {
+                name: "Pasilyo",
+                artist: "SunKissed Lola",
+                img: "music-9",
+                src: "music-9"
+                },
+    {
+    name: "Warriors",
+    artist: "Imagine Dragons",
+    img: "music-10",
+    src: "music-10"
+    },
+    ];
